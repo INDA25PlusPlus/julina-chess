27dde581172f3603eb2639e1b5956680d3009d91
JulinaSavela <julina.savela@outlook.com>
2025-09-07 08:19:57 +0200

    feat: add rook_moves, working on #7

--- chess-api/src/legal_moves.rs
+++ chess-api/src/legal_moves.rs
@@ -1,3 +1,5 @@
+use crate::BOARD;
+
 // prevent moving outside 8x8-board
 pub fn is_valid_move(cur_square: i64, row_delta: i64, col_delta: i64) -> bool {
 
@@ -63,5 +65,51 @@ pub fn king_moves(square: i64) -> u64 { // add more checks later (for check, che
 
     return targeted_squares;
 
+
+}
+
+
+pub fn rook_moves(square: i64) {
+
+    let mut board = BOARD.lock().unwrap();
+
+    let mut targeted_squares: u64 = 0u64;
+
+    let dirs: [(i64, i64); 4] = [
+        (-1, 0), // down (bit 0 represents sqr a1)
+        (1, 0), // up
+        (0, -1), // left
+        (0, 1) // right
+
+    ];
+
+
+    for (row_delta, col_delta) in dirs {
+    
+        let n = 1; // num steps in direction (row_delta, col_delta)
+
+        while n < 8 {
+
+            if is_valid_move(square, row_delta*n, col_delta*n) {
+                // FIX: ADD MOVE DEPENDING ON MOVE/COLOR (if the piece can be captured, or if it's your own piece)
+                if (board.black_occupied | board.white_occupied) & (1 << square+row_delta*8*n+col_delta*n) == 0 { // not occupied
+
+                    targeted_squares |= 1 << square+row_delta*8*n+col_delta*n;
+                }   
+                else {
+                    break;
+                }
+            }
+
+            else {
+                break;
+            }
+
+            n += 1;
+        }
+    }
+
+    return targeted_squares;
+        
 
 }
