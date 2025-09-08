0f8e94b672484e8860d7b5aadffe3b71222f90a6
JulinaSavela <julina.savela@outlook.com>
2025-09-08 19:17:19 +0200

    feat: add make_move(), fix #12

--- chess-api/src/perform_moves.rs
+++ chess-api/src/perform_moves.rs
@@ -10,7 +10,11 @@ use crate::MOVE;
 
 
 
-pub fn is_legal(cur_square: i64, target_square: i64) -> bool {
+pub fn is_legal(cur_square: i8, target_square: i8) -> bool {
+
+    if cur_square < 0 || cur_square > 63 || target_square < 0 || target_square > 63 {
+        return false;
+    }
 
     let board = BOARD.lock().unwrap();
 
@@ -37,14 +41,130 @@ pub fn is_legal(cur_square: i64, target_square: i64) -> bool {
 }
 
 
-pub fn read_move(cur_square: String, target_square: String) {
+// pub fn read_move(cur_square: String, target_square: String) {
 
-    // convert string to bit (and check if invalid cur_square or target_square)
-
-
-}
+//     // convert string to bit (and check if invalid cur_square or target_square)
 
 
-pub fn make_move(cur_square: u8, target_square: u8) {
+
+
+// }
+
+
+pub fn make_move(cur_square: i8, target_square: i8) {
+
+    let mut board = BOARD.lock().unwrap();
+    let mut to_move = MOVE.lock().unwrap();
+
+    if !is_legal(cur_square, target_square) {
+        print!("{}", "Invalid move.");
+        return;
+    }
+
+    let cur_mask: u64 = 1<<cur_square;
+    let target_mask: u64 = 1<<target_square;
+
+
+    // remove piece from current square
+    board.white_occupied &= !cur_mask;
+    board.white_pawns &= !cur_mask;
+    board.white_king &= !cur_mask;
+    board.white_queens &= !cur_mask;
+    board.white_knights &= !cur_mask;
+    board.white_rooks &= !cur_mask;
+    board.white_bishops &= !cur_mask;
+
+    board.black_occupied &= !cur_mask;
+    board.black_pawns &= !cur_mask;
+    board.black_king &= !cur_mask;
+    board.black_queens &= !cur_mask;
+    board.black_knights &= !cur_mask;
+    board.black_rooks &= !cur_mask;
+    board.black_bishops &= !cur_mask;
+
+    // add piece to target square
+    if target_mask & board.white_pawns != 0 {
+        board.white_occupied |= target_mask;
+        board.white_pawns |= target_mask;
+    }
+
+    if target_mask & board.white_king != 0 {
+        board.white_occupied |= target_mask;
+        board.white_king |= target_mask;
+    }
+
+    if target_mask & board.white_queens != 0 {
+        board.white_occupied |= target_mask;
+        board.white_queens |= target_mask;
+    }
+
+    if target_mask & board.white_knights != 0 {
+        board.white_occupied |= target_mask;
+        board.white_knights |= target_mask;
+    }
+
+    if target_mask & board.white_rooks != 0 {
+        board.white_occupied |= target_mask;
+        board.white_rooks |= target_mask;
+    }
+
+    if target_mask & board.white_bishops != 0 {
+        board.white_occupied |= target_mask;
+        board.white_bishops |= target_mask;
+    }
+
+    if target_mask & board.black_pawns != 0 {
+        board.black_occupied |= target_mask;
+        board.black_pawns |= target_mask;
+    }
+
+    if target_mask & board.black_king != 0 {
+        board.black_occupied |= target_mask;
+        board.black_king |= target_mask;
+    }
+
+    if target_mask & board.black_queens != 0 {
+        board.black_occupied |= target_mask;
+        board.black_queens |= target_mask;
+    }
+
+    if target_mask & board.black_knights != 0 {
+        board.black_occupied |= target_mask;
+        board.black_knights |= target_mask;
+    }
+
+    if target_mask & board.black_rooks != 0 {
+        board.black_occupied |= target_mask;
+        board.black_rooks |= target_mask;
+    }
+
+    if target_mask & board.black_bishops != 0 {
+        board.black_occupied |= target_mask;
+        board.black_bishops |= target_mask;
+    }
+
+
+    // if capture
+    if (target_mask & board.black_occupied) != 0 {
+        board.black_occupied &= !target_mask;
+        board.black_pawns &= !target_mask;
+        board.black_king &= !target_mask;
+        board.black_queens &= !target_mask;
+        board.black_knights &= !target_mask;
+        board.black_rooks &= !target_mask;
+        board.black_bishops &= !target_mask;
+    }
+    if (target_mask & board.white_occupied) != 0 {
+        board.white_occupied &= !target_mask;
+        board.white_pawns &= !target_mask;
+        board.white_king &= !target_mask;
+        board.white_queens &= !target_mask;
+        board.white_knights &= !target_mask;
+        board.white_rooks &= !target_mask;
+        board.white_bishops &= !target_mask;
+    }
+
+    // toggle turns
+    *to_move = (*to_move+1)%2;
 
 }
