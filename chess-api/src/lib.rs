0b5de222c8e92c0255f5351bcfd032ee4b2ce9cd
JulinaSavela <julina.savela@outlook.com>
2025-09-11 19:34:48 +0200

    feat: add game(), read input

--- chess-api/src/lib.rs
+++ chess-api/src/lib.rs
@@ -6,7 +6,6 @@ pub mod perform_moves;
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 use bitboards::Board;
-//use std::io::{stdin,stdout,Write};
 
  // GLOBAL STATES
 pub static BOARD: Lazy<Mutex<Board>> = Lazy::new(|| {
@@ -17,36 +16,53 @@ pub static MOVE: Lazy<Mutex<u8>> = Lazy::new(|| { // 0: whites move, 1: blacks m
     Mutex::new(0)
 });
 
-// fn game() {
+#[allow(dead_code)]
+fn game() {
 
-//     bitboards::print_board();
+    // take input in rust: https://users.rust-lang.org/t/how-to-get-user-input/5176/3
+    use std::io::{stdin,stdout,Write};
 
-//     if *MOVE.lock().unwrap() == 0 {
-//         print!("{}", "White to move. Make a move by printing current move, followed by target move (on different lines).\n");
-//     }
-//     else {
-//         print!("{}", "Black to move. Make a move.\n");
-//     }
+    let mut s = String::new();
 
 
-//     let mut m:i8 = 0;
+    loop {
+        print!("Enter q to quit. Else press enter.\n");
+        let _=stdout().flush();
+        stdin().read_line(&mut s).expect("Did not enter a correct string");
+
+        if let Some('\n')=s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r')=s.chars().next_back() {
+            s.pop();
+        }
+
+        if s == "q".to_string() {
+            break;
+        }
+
+
+        println!("You typed: {}",s);
+
+
+        
+
+        s.clear();
+
+         //perform_moves::read_move(current, target);
+
+            
+
+
+
+        }
+
+
+    }
+
+
+
     
-//     loop {
-//         print!("{}", "Print q to quit the game. Else continue playing. \n");
-
-//         stdout().flush().expect("failed to flush stdout");
-//         m.clear();
-//         stdin().read_line(&mut m).expect("Did not enter a correct string")
-
-
-
-//     }
-
-   
-
-
-
-// }
 
 #[allow(dead_code)] // (only used in tests)
 fn dbg_print_board(bb: u64) { // for debugging and testing
@@ -70,7 +86,7 @@ fn dbg_print_board(bb: u64) { // for debugging and testing
 #[cfg(test)]
 mod tests {
     // use super::*;
-    use crate::{bitboards, dbg_print_board, legal_moves, perform_moves, BOARD};
+    use crate::{bitboards, dbg_print_board, legal_moves, perform_moves, BOARD, game};
 
     
 
@@ -99,29 +115,34 @@ mod tests {
     //     print!("{}", "\n");
     // }
 
+    // #[test]
+    // fn test_make_move() {
+    //     //perform_moves::is_legal(8, 16); // legal
+    //     perform_moves::make_move(8, 24);
+
+    //     // print board
+    //     bitboards::print_board();
+
+
+    //     perform_moves::make_move(24, 40); // not legal
+
+    //     bitboards::print_board();
+
+    //     perform_moves::make_move(48, 40);
+
+    //     let board = BOARD.lock().unwrap();
+    //     dbg_print_board(board.white_pawns);
+    //     drop(board);
+    //     dbg_print_board(legal_moves::pawn_moves(24));
+
+
+    //     // print board
+    //     bitboards::print_board();
+    // }
+
     #[test]
-    fn test_make_move() {
-        //perform_moves::is_legal(8, 16); // legal
-        perform_moves::make_move(8, 24);
-
-        // print board
-        bitboards::print_board();
-
-
-        perform_moves::make_move(24, 40); // not legal
-
-        bitboards::print_board();
-
-        perform_moves::make_move(48, 40);
-
-        let board = BOARD.lock().unwrap();
-        dbg_print_board(board.white_pawns);
-        drop(board);
-        dbg_print_board(legal_moves::pawn_moves(24));
-
-
-        // print board
-        bitboards::print_board();
+    fn test_game_take_input() {
+        game();
     }
 
     
